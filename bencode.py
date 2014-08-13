5451ed74c861274acdadebecfdf29c389071e3dd
Rachel King <rachel.b.king@gmail.com>
2014-08-13 12:39:50 -0400

    Modified bencode, added tests.

--- bencode.py
+++ bencode.py
@@ -1,39 +1,75 @@
 import re
-try:
-    import psyco # Optional, 2.5x improvement in speed
-    psyco.full()
-except ImportError:
-    pass
+import numbers
+import logging
 
 decimal_match = re.compile('\d')
 
 def bdecode(data):
-    '''Main function to decode bencoded data'''
+    '''
+    Main function to decode bencoded data
+    Returns list of tuples of key/value pairs
+    '''
     chunks = list(data)
     chunks.reverse()
     root = _dechunk(chunks)
     return root
+
+def bencode(data):
+    '''
+    Main function to bencode data
+    Accepts list of tuples of key/value pairs
+    Returns bencoded data--do I need to worry about making it a bytestring?
+    '''
+    if type(data) is list:
+        #Making an assumption below that if we're in one of our lists of tuples, ALL first-level elements are going to be tuples. Is that an okay assumption? Otherwise, how to check that ALL elements of the list are tuples (with two elements each, if extra detail is needed)?
+        if type(data[0]) is tuple:
+            tmp = 'd'
+            for element in data:
+                key = element[0]
+                value = element[1]
+                tmp += bencode(key) + bencode(value)
+            tmp += 'e'
+            return tmp
+        else:
+            tmp = 'l'
+            for element in data:
+                tmp += bencode(element)
+            tmp += 'e'
+            return tmp
+
+    elif isinstance(data, numbers.Real):
+        #This checks if data is an int, long, or float.
+        tmp = 'i' + str(int(data)) + 'e'
+        return tmp
+
+    elif isinstance(data, basestring):
+        tmp = str(len(data)) + ':' + data
+        return tmp
+
+    else:
+        logging.error('Unexpected data type for bencoding.')
+
 
 def _dechunk(chunks):
     item = chunks.pop()
 
     if item == 'd': 
         item = chunks.pop()
-        hash = {}
+        temp_array = []
         while item != 'e':
             chunks.append(item)
             key = _dechunk(chunks)
-            hash[key] = _dechunk(chunks)
+            temp_array.append((key, _dechunk(chunks)))
             item = chunks.pop()
-        return hash
+        return temp_array
     elif item == 'l':
         item = chunks.pop()
-        list = []
+        temp_list = []
         while item != 'e':
             chunks.append(item)
-            list.append(_dechunk(chunks))
+            temp_list.append(_dechunk(chunks))
             item = chunks.pop()
-        return list
+        return temp_list
     elif item == 'i':
         item = chunks.pop()
         num = ''
--- tests/test_hummus.py
+++ tests/test_hummus.py
@@ -1,27 +1,46 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 from nose.tools import eq_
+import logging
 
 import socket
 from threading import Thread, Lock
+import hashlib
 
-from hummus.utilities import connectToPeer
-from hummus.peer import Peer.send, Peer.recv
+import hummus.utilities as utilities
+import hummus.bencode as bencode
 
 INVALID_PORT = 6889
 LOCALHOST_PORT = 6885
 clientsocket_lock = Lock()
 address_lock = Lock()
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('localhost', LOCALHOST_PORT))
+serversocket.listen(5)
 
-def runServer(self):
-    global clientsocket_lock, address_lock
+bencoded_info = u'd5:filesld6:lengthi764393e4:pathl89:029_7daystoeasymoneygetpaidtowriteabook_Sjhd Free Plr Mrr Article Download___________.exeeed6:lengthi291e4:pathl27:Distributed by Mininova.txteee4:name76:029_7daystoeasymoneygetpaidtowriteabook_Sjhd Free Plr Mrr Article Download_ 12:piece lengthi1048576e6:pieces20:/ZPl ‚k}³ôÃŸ–CA¼Ð 6e'
+
+info = [(u'files', 
+                        [
+                            [(u'length', 764393), 
+                             (u'path', [u'029_7daystoeasymoneygetpaidtowriteabook_Sjhd Free Plr Mrr Article Download___________.exe'])], 
+                            [(u'length', 291), 
+                             (u'path', [u'Distributed by Mininova.txt'])]
+                        ]
+                ), 
+                (u'name', u'029_7daystoeasymoneygetpaidtowriteabook_Sjhd Free Plr Mrr Article Download_ '), 
+                (u'piece length', 1048576), 
+                (u'pieces', u'/ZPl ‚k}³ôÃŸ–CA¼Ð 6')]
+
+def runServer():
+    global clientsocket_lock, address_lock, serversocket
     with clientsocket_lock:
         with address_lock:
             (clientsocket, address) = serversocket.accept()
 
 def setUp():
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind(('localhost', LOCALHOST_PORT))
-    serversocket.listen(5)
+    logging.basicConfig(level=logging.INFO)
     t = Thread(target=runServer)
     t.daemon = True
     t.start()
@@ -29,31 +48,46 @@ def setUp():
 def tearDown():
     serversocket.close()
 
-class TestGlobalFunctions(unittest.TestCase):
+class TestBencode(unittest.TestCase):
+    def test_bencode(self):
+        bencode_test = bencode.bencode(info)
+        eq_(bencode_test, bencoded_info)
+
+    def test_bdecode(self):
+        bdecode_test = bencode.bdecode(bencoded_info)
+        eq_(bdecode_test, info)
+
+class TestUtilities(unittest.TestCase):
     def test_connectToPeer_valid(self):
-        socket = connectToPeer('localhost', LOCALHOST_PORT)
+        socket = utilities.connectToPeer('localhost', LOCALHOST_PORT)
         eq_(socket.getpeername(), ('127.0.0.1', LOCALHOST_PORT))
 
     def test_connectToPeer_invalid(self):
-        socket = connectToPeer('localhost', INVALID_PORT)
+        socket = utilities.connectToPeer('localhost', INVALID_PORT)
         eq_(socket, None)
+
+    def test_constructHandshake(self):
+        pass
+
+    def test_parseHandshake(self):
+        pass
         
-class TestCreatePeer(unittest.TestCase):
-    def setUp(self):
-        self.manager = {}
-    def test_create_peer(self):
-        peer = Peer(peer_id, self.manager)
+# class TestCreatePeer(unittest.TestCase):
+#     def setUp(self):
+#         self.manager = {}
+#     def test_create_peer(self):
+#         peer = Peer(peer_id, self.manager)
 
-class TestPeerMethods(unittest.TestCase):
-    # def setUp(self):
-    #     test_peer1 = Peer()
-    #     test_peer2 = Peer()
+# class TestPeerMethods(unittest.TestCase):
+#     def setUp(self):
+#         test_peer1 = Peer()
+#         test_peer2 = Peer()
 
-    def test_send(self):
-        class TestSocket(object):
-            def __init__(self):
-        eq_()
-    def test_recv(self):
+#     def test_send(self):
+#         class TestSocket(object):
+#             def __init__(self):
+#         eq_()
+#     def test_recv(self):
 
-        eq_()
+#         eq_()
 
